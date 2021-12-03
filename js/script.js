8bd8bcdee2dba789f284b9c3cc0dbc10eb0050ac
Samuel Scozzari <s_scozzari@libero.it>
2021-12-03 14:11:03 +0100

    set-up of the reasoning

---
+++ js/script.js
@@ -0,0 +1,17 @@
+/* 
+Richiesta del prezzo totale di un viaggio - info:
+
+1) Il programma deve richiedere all'utente il numero di km da percorrere e la sua età.
+- In questo caso si sta parlando di due prompt in let.
+
+2) Il prezzo del biglietto è definito in base ai km (0.21€ al km);
+- è necessario definire una variabile in let posta a racchiudere il valore derivato 
+dal calcolo del prezzo in base ai km, senza considerare gli sconti suddivisi per fasce d'età
+
+3) Vanno applicate due differenti tipologie di sconto in base all'età del passeggero. 
+- Il tutto deve strutturarsi teoricamente all'interno di un if, un else if e un else. 
+
+4) Il risultato deve essere stampato a schermo
+- il risultato deve essere approssimato a massimo 2 decimali, al fine di indicare i centesimi sul prezzo.
+- stabilire un collegamento con l'elemento html che presenterà il risultato.
+*/
